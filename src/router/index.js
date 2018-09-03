0f6d6ca10b360d3f21bf9da387d41b2946e3741c
psj <1373999235@qq.com>
2018-09-03 20:17:18 +0800

    第二次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+// 配置路由
+// @代表src 在build文件中配置
+import Login from '@/views/Login';
 
-Vue.use(Router)
+// import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+  routes: [{
+    path: '/login',
+    name: 'login',
+    component: Login
+  }]
+});
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login-wrap">
+    <el-form label-position="top" label-width="80px">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+      <el-button class="login-btn" type="primary">登录</el-button>
+    </el-form>
+  </div>
+</template>
+<script>
+</script>
+<style>
+  .login-wrap {
+    background: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-form {
+    width: 500px;
+    padding: 30px;
+    border-radius: 5px;
+    background: #fff;
+  }
+
+  .login-btn {
+    width: 100%;
+  }
+
+</style>
